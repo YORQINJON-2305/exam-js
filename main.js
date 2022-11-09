cc5a79d69d3b3d2e4aefaa973b70aeb8d85cb7f2
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-11-09 12:00:53 +0500

    Bug fixed

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
+//get select form
 const elForm = document.querySelector(".test-form");
-
-//get select tags in main page
 const elLevelSelect = elForm.querySelector(".level-select");
 const elTimeSelect = elForm.querySelector(".time-select");
 
 //get test tags
-const elCloseTestModalBtn = document.querySelector(".close-test-modal");
-const elScoreText = document.querySelector(".user-score");
-const elCountAttempt = document.querySelector(".count-attempt");
-const elTime = document.querySelector(".test-time");
 const elTestModal = document.querySelector(".test-modal");
+const elScoreText = elTestModal.querySelector(".user-score");
+const elCountAttempt = elTestModal.querySelector(".count-attempt");
+const elTime = elTestModal.querySelector(".test-time");
 const elQuestionText = elTestModal.querySelector(".question-text");
 const elQuestionCounter = elTestModal.querySelector(".question-counter");
 const elSignList = elTestModal.querySelector(".sign-list");
+
+// get template
 const elSignTemplate = document.querySelector(".img-template").content;
 
 //get game over tags
@@ -26,16 +26,20 @@ const elUserResult = document.querySelector(".user-result-text");
 
 // get all sounds
 const trueSound = new Audio("./sounds/true.mp3");
-const falseSound = new Audio("./sounds/false.mp3");
+const falseSound = new Audio("./sounds/false-classic.mp3");
 const winnerSound = new Audio("./sounds/win.wav");
-const gameOverSound = new  Audio("./sounds/game-over-two.ogg")
+const gameOverSound = new  Audio("./sounds/game-over-mario.mp3")
 
 //Global fragment
 const globalFragment = document.createDocumentFragment();
 
+//Symbols image array
 const availableQuestions = [];
+
+//Questions array
 const availableQuestionsClone = [];
 
+//Form submit
 elForm.addEventListener("submit", function (evt){
     evt.preventDefault();
     document.body.classList.add("show");
@@ -45,10 +49,12 @@ elForm.addEventListener("submit", function (evt){
     timeFunction(elTimeSelect.value - 0);
 });
 
+//Random array
 let randomArrEasy = Math.floor(Math.random() * 80)
 let randomArrNormal = Math.floor(Math.random() * 59)
 let randomArrHard = Math.floor(Math.random() * 38)
 
+//Level select
 function levelFind(levelSelect){
     if(levelSelect === "easy"){
       const easySymbols = roadSymbols.splice(randomArrEasy, 21);
@@ -73,6 +79,7 @@ function levelFind(levelSelect){
     }
 }
 
+//Time Function
 function timeFunction(selectValue){
     let counter = selectValue;
     displayTime(counter);
@@ -86,13 +93,17 @@ function timeFunction(selectValue){
     }, 1000);
 }
 
+//Time display show
 function displayTime(second){
     const min = Math.floor(second / 60);
     const sec = Math.floor(second % 60);
     elTime.innerHTML = `${min < 10 ? '0': ''}${min}:${sec < 10 ? '0' : ''}${sec}`
 }
 
+//Question counter
 let questionCounter = 0;
+
+//Get random question
 function getNewQuestion(){
     elQuestionCounter.innerHTML = `Question ${questionCounter + 1} of ${availableQuestions.length}`;
     if(questionCounter === availableQuestions.length){
@@ -108,10 +119,17 @@ function getNewQuestion(){
     availableQuestionsClone.splice(indexOne, 1);
 }
 
+
+//Event delegation symbols list
 elSignList.addEventListener("click", findTrueQuestion);
 
+//Score Counter
 let scoreCounterText = 0;
+
+//Attempt counter
 let countAttempt = 5;
+
+//Event delegation function
 function findTrueQuestion(evt){
     const getQuestionId = Number(elQuestionText.dataset.id);
     let foundQuestion;
@@ -146,6 +164,7 @@ function findTrueQuestion(evt){
     counterEnd();
 };
 
+//Game over function
 function counterEnd(){
     if(scoreCounterText === -5){
         elTestModal.classList.add("d-none");
@@ -159,6 +178,7 @@ function counterEnd(){
     }
 }
 
+//Render Symbols Image
 function renderSignImg(allSymbols){
     elSignList.innerHTML = "";
     allSymbols.forEach(item => {
@@ -172,7 +192,7 @@ function renderSignImg(allSymbols){
     elSignList.appendChild(globalFragment);
 }
 
-//Replay btn
+//Winner replay btn
 elWinnerBtn.addEventListener("click", function (){
     window.location.reload();
 });
