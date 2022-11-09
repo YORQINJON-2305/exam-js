2514583d79f6b17e549e722476a64ef6ef0ed56c
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-11-09 09:54:36 +0500

    Road symbols is over

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const elForm = document.querySelector(".test-form");
 
-//getselect tags in main page
+//get select tags in main page
 const elLevelSelect = elForm.querySelector(".level-select");
 const elTimeSelect = elForm.querySelector(".time-select");
 
 //get test tags
 const elCloseTestModalBtn = document.querySelector(".close-test-modal");
 const elScoreText = document.querySelector(".user-score");
+const elCountAttempt = document.querySelector(".count-attempt");
 const elTime = document.querySelector(".test-time");
 const elTestModal = document.querySelector(".test-modal");
 const elQuestionText = elTestModal.querySelector(".question-text");
@@ -18,10 +19,20 @@ const elSignTemplate = document.querySelector(".img-template").content;
 const elGameOverWrap = document.querySelector('.game-over-wrap');
 const elRefreshBtn = elGameOverWrap.querySelector('.refresh-btn');
 
+//get winner modal tags
+const elWinnerModal = document.querySelector(".winner-wrap");
+const elWinnerBtn = document.querySelector(".replay-btn");
+const elUserResult = document.querySelector(".user-result-text");
+
+// get all sounds
+const trueSound = new Audio("./sounds/true.mp3");
+const falseSound = new Audio("./sounds/false.mp3");
+const winnerSound = new Audio("./sounds/win.wav");
+const gameOverSound = new  Audio("./sounds/game-over.wav")
+
 //Global fragment
 const globalFragment = document.createDocumentFragment();
 
-let currentQuestion;
 const availableQuestions = [];
 const availableQuestionsClone = [];
 
@@ -34,34 +45,33 @@ elForm.addEventListener("submit", function (evt){
     timeFunction(elTimeSelect.value - 0);
 });
 
-// let randomArr = Math.floor(Math.random() * 80)
+let randomArrEasy = Math.floor(Math.random() * 80)
+let randomArrNormal = Math.floor(Math.random() * 59)
+let randomArrHard = Math.floor(Math.random() * 38)
+
 function levelFind(levelSelect){
     if(levelSelect === "easy"){
-      const easySymbols = roadSymbols.splice(0, 21);
+      const easySymbols = roadSymbols.splice(randomArrEasy, 21);
       easySymbols.forEach(item => {
           availableQuestions.push(item);
           availableQuestionsClone.push(item);
       })
     }
     if(levelSelect === "normal"){
-        const normalSymbols = roadSymbols.splice(0, 41);
+        const normalSymbols = roadSymbols.splice(randomArrNormal, 42);
         normalSymbols.forEach(item => {
             availableQuestions.push(item);
             availableQuestionsClone.push(item);
         })
     }
     if(levelSelect === "hard"){
-        const hardSymbols = roadSymbols.splice(0, 63);
+        const hardSymbols = roadSymbols.splice(randomArrHard, 63);
         hardSymbols.forEach(item => {
             availableQuestions.push(item);
             availableQuestionsClone.push(item);
         });
     }
 }
-
-elCloseTestModalBtn.addEventListener("click", function (){
-    document.body.classList.remove("show");
-});
 
 function timeFunction(selectValue){
     let counter = selectValue;
@@ -82,75 +92,79 @@ function displayTime(second){
     elTime.innerHTML = `${min < 10 ? '0': ''}${min}:${sec < 10 ? '0' : ''}${sec}`
 }
 
-elSignList.addEventListener("click", findTrueQuestion);
-
 let questionCounter = 0;
 function getNewQuestion(){
     elQuestionCounter.innerHTML = `Question ${questionCounter + 1} of ${availableQuestions.length}`;
     if(questionCounter === availableQuestions.length){
-        elGameOverWrap.classList.add("d-flex");
+        elWinnerModal.classList.add("d-flex");
+        winnerSound.play();
     }
     // get random questions
-    const questionIndex = availableQuestionsClone[Math.floor(Math.random() * availableQuestions.length)];
-    elQuestionText.textContent = questionIndex.symbol_title;
+    const questionIndex = availableQuestionsClone[Math.floor(Math.random() * availableQuestionsClone.length)];
     elQuestionText.dataset.id = questionIndex.id;
-    const indexOne = availableQuestionsClone.indexOf(questionIndex);
+    elQuestionText.textContent = questionIndex.symbol_title;
+    questionCounter++;
+    const indexOne = availableQuestionsClone.findIndex(item => item.id === questionIndex.id);
     availableQuestionsClone.splice(indexOne, 1);
-    questionCounter++
 }
 
+elSignList.addEventListener("click", findTrueQuestion);
+
 let scoreCounterText = 0;
+let countAttempt = 5;
 function findTrueQuestion(evt){
     const getQuestionId = Number(elQuestionText.dataset.id);
-    let findQ;
+    let foundQuestion;
     if(evt.target.matches(".question-item")){
         const getSignId = Number(evt.target.dataset.id);
         const findQuestion = availableQuestions.find(item => {
             return  getSignId === item.id;
         });
-        findQ = findQuestion
+        foundQuestion = findQuestion
     }
-    if(findQ.id === getQuestionId){
+    if(foundQuestion.id === getQuestionId){
         getNewQuestion();
-        findQ.isComplete = true;
-        renderSignImg(availableQuestions);
+        evt.target.classList.add("true-answer");
+        setTimeout(() => {
+            evt.target.classList.add("sign-hidden");
+        }, 1600);
+        trueSound.play();
         let twoNumber = 2
         scoreCounterText += twoNumber;
     }  else{
-        scoreCounterText--
+        evt.target.classList.add("false-answer");
+        setTimeout(() => {
+            evt.target.classList.remove("false-answer")
+        }, 2500);
+        scoreCounterText--;
+        countAttempt--;
+        falseSound.play();
     }
     elScoreText.textContent = scoreCounterText;
-    counterEnd()
+    elCountAttempt.textContent = countAttempt;
+    elUserResult.textContent = scoreCounterText;
+    counterEnd();
 };
 
 function counterEnd(){
     if(scoreCounterText === -5){
         elTestModal.classList.add("d-none");
-        elGameOverWrap.classList.add("d-flex")
+        elGameOverWrap.classList.add("d-flex");
+        gameOverSound.play();
+    }
+    if(countAttempt === 0){
+        elTestModal.classList.add("d-none");
+        elGameOverWrap.classList.add("d-flex");
+        gameOverSound.play();
     }
 }
 
 function renderSignImg(allSymbols){
     elSignList.innerHTML = "";
 
-
     allSymbols.forEach(item => {
-        // const newItem = document.createElement("li");
-        // newItem.classList.add("question-item");
-        // newItem.dataset.id = item.id;
-        // const newImg = document.createElement("img");
-        // newImg.src = item.symbol_img;
-        // newImg.classList.add("sign-img", "img-fluid");
-        // const newCheckImg = document.createElement("img");
-
-
-
-
         const signTemplateClone = elSignTemplate.cloneNode(true);
         signTemplateClone.querySelector(".question-item").dataset.id = item.id;
-        if(item.isComplete === true){
-            signTemplateClone.querySelector(".question-item").style.backgroundColor = "green";
-        }
         signTemplateClone.querySelector(".sign-img").src = item.symbol_img;
 
         globalFragment.appendChild(signTemplateClone);
@@ -159,16 +173,15 @@ function renderSignImg(allSymbols){
     elSignList.appendChild(globalFragment);
 }
 
-
+//Replay btn
+elWinnerBtn.addEventListener("click", function (){
+    window.location.reload();
+})
 
 function endTime(){
     elTestModal.classList.add("d-none");
-    elGameOverWrap.classList.add("d-flex")
+    elGameOverWrap.classList.add("d-flex");
+    gameOverSound.play();
 };
-
-elRefreshBtn.addEventListener("click", function (){
-    window.location.reload();
-});
-
 
 
